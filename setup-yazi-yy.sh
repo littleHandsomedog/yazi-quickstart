d137691890e028a114f0a1a82fe1b9fbb39a0f3b
Handsomedog <2870752103@qq.com>
2025-08-10 01:04:24 +0800

    feat: 更新 yazi 安装脚本以支持 sudo 用户的环境变量设置

--- setup-yazi-yy.sh
+++ setup-yazi-yy.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 # 自动将 yazi 包装器 yy 函数添加到 .bashrc 或 .zshrc
 
+# 检查是否使用 sudo 启动脚本
+if [ "$EUID" -ne 0 ]; then
+    echo "请使用 sudo 启动此脚本。"
+    exit 1
+fi
+
+if [ -n "$SUDO_USER" ] && [ -z "$TARGET_USER" ]; then
+    TARGET_USER="$SUDO_USER"
+    TARGET_HOME=$(getent passwd "$SUDO_USER" | cut -d: -f6)
+fi
+
 YY_FUNC='function yy() {\n\tlocal tmp="$(mktemp -t \"yazi-cwd.XXXXXX\")" cwd\n\tyazi "$@" --cwd-file="$tmp"\n\tIFS= read -r -d '' cwd < "$tmp"\n\t[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"\n\trm -f -- "$tmp"\n}'
 
 add_func() {
@@ -15,5 +26,5 @@ add_func() {
 	fi
 }
 
-add_func "$HOME/.bashrc"
-add_func "$HOME/.zshrc"
+add_func "$TARGET_HOME/.bashrc"
+add_func "$TARGET_HOME/.zshrc"
