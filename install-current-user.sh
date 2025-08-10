0af426161de10010f44a226a78a8546adbfe8adf
Handsomedog <2870752103@qq.com>
2025-08-10 13:43:36 +0800

    fix: 修复yy包装器无法切换目录的bug

--- install-current-user.sh
+++ install-current-user.sh
@@ -146,17 +146,22 @@ ya pack -a dangooddd/kanagawa
 echo "kanagawa主题安装完成"
 
 # 添加YY包装器
-YY_FUNC='function yy() {\n\tlocal tmp="$(mktemp -t \"yazi-cwd.XXXXXX\")" cwd\n\tyazi "$@" --cwd-file="$tmp"\n\tIFS= read -r -d '' cwd < "$tmp"\n\t[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"\n\trm -f -- "$tmp"\n}'
-
+YY_FUNC='function yy() {
+    local tmp=$(mktemp) cwd
+    yazi "$@" --cwd-file="$tmp"
+    IFS= read -r -d "" cwd < "$tmp"
+    [ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+    rm -f "$tmp"
+}'
 add_func() {
 	local rcfile="$1"
 	if [ -f "$rcfile" ]; then
-		if ! grep -q "function yy()" "$rcfile"; then
-			echo -e "\n$YY_FUNC\n" >>"$rcfile"
-			echo "已添加 yy 函数到 $rcfile"
-		else
-			echo "$rcfile 已包含 yy 函数，无需重复添加。"
-		fi
+        if grep -F "function yy()" "$rcfile" > /dev/null; then
+            echo "$rcfile 已包含 yy 函数，跳过。"
+        else
+            printf '%s\n' "" "$YY_FUNC" "" >> "$rcfile"
+            echo "✅ 已添加 yy 函数到 $rcfile"
+        fi
 	fi
 }
 
